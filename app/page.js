e904f8f4fb90e29bc6de6f1a07895520c0f0de8e
Quantaindew <35188255+Quantaindew@users.noreply.github.com>
2023-08-20 13:08:46 +0530

    works! stable

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 import Link from "next/link";
-import Header from '@/components/Header';
+import Header from '../components/Header';
 
 export default function Home() {
   return (
